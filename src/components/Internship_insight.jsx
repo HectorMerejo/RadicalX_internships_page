0b3234f82151eb3977ea69e990dc7e156650e4d5
Hector Merejo <64796691+HectorMerejo@users.noreply.github.com>
2024-01-13 16:52:34 -0500

    Update Internship_insight.jsx

--- src/components/Internship_insight.jsx
+++ src/components/Internship_insight.jsx
@@ -2,27 +2,32 @@ import { useState } from 'react'
 import calendar from '../assets/calendar-2.png'
 import funnel from '../assets/Funnel.png'
 
-export default function Internships(){
-  return(
+export default function Internships() {
+  const [selectedDates, setSelectedDates] = useState([]);
+  const handleSelectDates = () => {
+  const selectedDatesFromPicker = datepicker.getSelectedDates();
+
+  setSelectedDates(selectedDatesFromPicker);
+  console.log('Selected Dates:', selectedDatesFromPicker);
+  };
+
+  return (
     <div className='middiv--rightdiv'>
       <div className='middiv--insights'>
         <h5>Internship Insights</h5>
-        <p>In the eighteenth century the 
-          German philosopher Immanuel Kant 
-          developed a theory of knowledge 
-          in which knowledge about space 
-          can be both a priori and synthetic.
+        <p>
+          In the eighteenth century the German philosopher Immanuel Kant developed a theory of knowledge
+          in which knowledge about space can be both a priori and synthetic.
         </p>
-
       </div>
       <div className='middiv--headerdiv'>
-
         <button className='button1'>This week</button>
         <button className='button2'>This month</button>
-        <button className='button3'><img src={calendar} alt="calendar" />Select dates</button>
+        <button className='button3' onClick={handleSelectDates}>
+          <img src={calendar} alt="calendar" />Select dates
+        </button>
         <img src={funnel} alt="funnel" />
       </div>
-      
     </div>
-  )
+  );
 }
